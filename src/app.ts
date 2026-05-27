a55d9d9b000c7b6b62f87e261105bf4db6090b69
Carol <98452808+Lanus311@users.noreply.github.com>
2026-05-27 07:14:41 +0000

    create ts type failure

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import Fastify, { FastifyServerOptions } from 'fastify';
 
 export function buildApp(options: FastifyServerOptions = {}) {
+  const x: string = 123;
+  
   const app = Fastify({
     logger: options.logger ?? true,
     ...options
